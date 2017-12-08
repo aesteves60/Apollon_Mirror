f73f38036593e9a6a02547e4ec0e28f0e76c4eb7
Unknown <jeanchris.declunder@gmail.com>
2017-12-08 09:51:10 +0100

    Meteo : ok sans CSS

--- src/app/mirror/meteo/meteo.component.ts
+++ src/app/mirror/meteo/meteo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { jourMeteo } from './jourMeteo';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map'; 
 
 @Component({
   selector: 'app-meteo',
@@ -7,18 +11,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./meteo.component.css']
 })
 export class MeteoComponent implements OnInit {
+  tabMeteo: jourMeteo[] = [] ;
+  currentUrl: string;
+  constructor( private http: HttpClient,private router:Router) { }
 
-  constructor( private http: HttpClient) { }
-  getMeteo() {
-    this.http.get('https://www.prevision-meteo.ch/services/json/lat=46.259lng=5.235')
-      .subscribe((res: Response) => {
-        const meteo = res;
-        console.log(meteo);
-      })
-  }
+
+  
   ngOnInit() {
-    this.getMeteo();
+    this.currentUrl = this.router.url;
+    var obj:any ;
+       this.http.get('https://www.prevision-meteo.ch/services/json/lat=46.259lng=5.235')
+     .subscribe(res => 
+       this.traiteData(res)
+      );
   }
+  traiteData(res){
+    /*
+    
+    tabMeteo => 0 condition actuelle
+    tabMeteo => 1 condition de la journée actuelle
+    tabMeteo => 1+n condition futur
+     */
+    console.log(res);
 
+    var compteur = 0;
+    for (let jour in res) {
+      if(jour=='current_condition'){
+        var today :jourMeteo = new jourMeteo(res[jour]['date'],res[jour]['condition'],res[jour]['condition_key'],null,null,res[jour]['tmp']);
+        this.tabMeteo.push(today);
+      }else if(jour=='city_info'){
+        continue;
+      }else if(jour=='fcst_day_'+compteur){
+        var day :jourMeteo = new jourMeteo(res['fcst_day_'+compteur]['date'],res['fcst_day_'+compteur]['condition'],
+                             res['fcst_day_'+compteur]['condition_key'], res['fcst_day_'+compteur]['tmax'],
+                             res['fcst_day_'+compteur]['tmin'],null);
+        this.tabMeteo.push(day);
+        compteur ++;
+      }
+    }
+    console.log(this.tabMeteo);
+  }
 
 }
