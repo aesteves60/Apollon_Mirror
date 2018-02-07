4e8bbed5a064915c5182ceafa368a6b7132d95d1
esteves <alexandre.esteves@pingflow.fr>
2018-02-07 09:45:07 +0100

    modif des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent }   from './login/login.component';
 import { ForgetPswComponent }   from './forgetpsw/forgetpsw.component';
-import { InscriptionComponent }   from './inscription/inscription.component';
 import { PersonalizeComponent }   from './personalize/personalize.component';
 import { MeteoComponent } from './mirror/meteo/meteo.component';
 import { MirrorComponent } from './mirror/mirror.component';
-import { AccountComponent } from './account/account.component';
 import { ContactComponent } from './contact/contact.component';
 import { AproposComponent } from './apropos/apropos.component';
 import { ApimanagerComponent } from './apimanager/apimanager.component';
@@ -19,20 +17,12 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'mirror', component: MirrorComponent },
   { path: 'meteo', component: MeteoComponent },
+  { path: 'apimanager', component: ApimanagerComponent  },
 	{ path: 'forgetpsw' , canActivate: [ AuthGuard ], component: ForgetPswComponent },
 	{ path: 'personalize', canActivate: [ AuthGuard ], component: PersonalizeComponent },
 	{ path: 'contacter' , canActivate: [ AuthGuard ], component: ContactComponent },
-	{ path: 'apropos' , canActivate: [ AuthGuard ], component: AproposComponent },
-	{ path: 'account' , canActivate: [ AuthGuard ], component: AccountComponent  }
-	{ path: 'forgetpsw', component: ForgetPswComponent },
-  { path: 'inscription', component: InscriptionComponent },
-	{ path: 'meteo', component: MeteoComponent },
-	{ path: 'personalize', component: PersonalizeComponent },
-	{ path: 'mirror', component: MirrorComponent },
-	{ path: 'contacter', component: ContactComponent },
-	{ path: 'apropos', component: AproposComponent },
-	{ path: 'account', component: AccountComponent  },
-	{ path: 'apimanager', component: ApimanagerComponent  }
+	{ path: 'apropos' , canActivate: [ AuthGuard ], component: AproposComponent }
+
 
 ];
 
