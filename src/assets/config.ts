131f9f0f4115cff278a0d2e042290112df8256bc
esteves <alexandre.esteves@pingflow.fr>
2018-08-20 15:06:05 +0200

    (config) changement de l'URL de l'API pour la prod

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,5 +1,5 @@
 export enum Config {
-  BASE_URL = 'http://localhost/Apipollon',
+  BASE_URL = 'http://149.202.45.167/Apipollon/',
   SERVER_SOCKETIO = 'http://localhost:3000',
   HTTP_MIDDLRWARE = 'http://localhost:8082',
   API_KEY = 'cb97bb13bf88861a088d65c08588810f',
